b92c883aeb86c5431b34370a3898f431877a6dfa
Vinicius-klaus <vinicius.genesio@usp.br>
2021-06-20 11:57:15 -0300

    feat(fonts): install font Inter and create a file with the fonts used in the app

--- App.tsx
+++ App.tsx
@@ -1,14 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {useFonts, 
+  Inter_100Thin, 
+  Inter_200ExtraLight,
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  Inter_900Black  
+} from '@expo-google-fonts/inter'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    Inter_100Thin, 
+    Inter_200ExtraLight,
+    Inter_300Light,
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+    Inter_900Black 
+  })
+
+  if(!fontsLoaded){
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
---
+++ src/styles/fonts.ts
@@ -0,0 +1,7 @@
+export default{
+    appName: 'Inter_900Black',
+    welcome: 'Inter_600SemiBold',
+    text: 'Inter_400Regular',
+    button: 'Inter_500Medium',
+    warning: 'Inter_600SemiBold'
+}
