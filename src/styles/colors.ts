06591e27fcf7b28201a42a5ea7bd59ebafde23eb
Vinicius-klaus <vinicius.genesio@usp.br>
2021-06-20 11:40:03 -0300

    feat(colors): create a style for the colors used in the project

---
+++ src/styles/colors.ts
@@ -0,0 +1,19 @@
+export default{
+    blue: '#1A73E9',
+    blue_light: '#1d3f74',
+    blue_dark1: '##313ce9',
+    blue_dark2: '#0A2239',
+    
+    green: '#1A936F',
+    green_light: '#1ae977',
+    green_dark: '#0C4534',
+    
+    gray: '#808189',
+    gray_light1: '#eaeaeb',
+    gray_light2: '#c6c7ca',
+    gray_dark1: '#6f7078',
+    gray_dark2: '#262629',
+    gray_dark3: '#0d0d0e',
+    
+    white: '#ffffff'
+}
