ea2871087bd88a86d5248dee222ca325ad67650d
Vinicius-klaus <vinicius.genesio@usp.br>
2021-07-08 16:51:29 -0300

    refactor(header): create Header simple component with just screen name and the go back lef-arrow
    change Header and HeaderWithOutMenu to contain HeaderSimple

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,37 +1,21 @@
-import React from 'react'
-import { Text, View, StyleSheet, Dimensions, TouchableOpacity } from 'react-native'
-import { MaterialIcons } from '@expo/vector-icons'; 
-import {SafeAreaView} from './SafeAreaView';
+import { MaterialIcons } from '@expo/vector-icons';
+import React from 'react';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { HeaderSimple } from '.';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
-import { useNavigation } from '@react-navigation/native';
 
 interface HeaderProps{
     titleScreen: string
 }
 export function Header({titleScreen}: HeaderProps){
-    const navigation = useNavigation()
-    
-    function handleGoBack(){
-        navigation.goBack()
-    }
+
     return(
         <View>
-            <View style={styles.header}>
-                <TouchableOpacity
-                    onPress={handleGoBack}
-                >
-                    <MaterialIcons style={styles.incons} name="chevron-left" size={30} color="white" />
-                </TouchableOpacity>
-                    <View style={styles.textScreenName}>
-                        <Text style={styles.textScreenName}>
-                            {titleScreen}
-                        </Text>
-                    </View>
-                
-                </View>
-            <View style={styles.container}>
-                
+            <HeaderSimple
+                titleScreen={titleScreen}
+            />
+            <View style={styles.container}>  
                 <MaterialIcons style={styles.incons} name="menu" size={24} color="black" />
                 <View style={styles.textContainer}>
                     <Text style={styles.textNameApp}>
@@ -53,21 +37,8 @@ const styles = StyleSheet.create({
       height: Dimensions.get('window').height * 0.15,
       justifyContent: 'center',
     },
-    header:{
-        width: '100%',
-        height: Dimensions.get('window').height * 0.05,
-        backgroundColor: colors.blue,
-        flexDirection: 'row'   
-    },
     incons: {
         paddingLeft: 5
-    },
-    textScreenName: {
-        fontFamily: fonts.generic,
-        fontSize: 20,
-        paddingLeft: 15,
-        color: colors.white,
-        justifyContent: 'space-around'
     },
     textNameApp:{
         fontSize: 32,
--- src/Components/HeaderWithOutMenu.tsx
+++ src/Components/HeaderWithOutMenu.tsx
@@ -1,35 +1,19 @@
-import React from 'react'
-import { Text, View, StyleSheet, Dimensions, TouchableOpacity } from 'react-native'
-import { MaterialIcons } from '@expo/vector-icons'; 
-import {SafeAreaView} from './SafeAreaView';
+import React from 'react';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
-import { useNavigation } from '@react-navigation/native';
+import { HeaderSimple } from './HeaderSimple';
 
 interface HeaderProps{
     titleScreen: string
 }
 export function HeaderWithOutMenu({titleScreen}: HeaderProps){
-    const navigation = useNavigation()
-    
-    function handleGoBack(){
-        navigation.goBack()
-    }
+
     return(
         <View>
-            <View style={styles.header}>
-                <TouchableOpacity
-                    onPress={handleGoBack}
-                >
-                    <MaterialIcons style={styles.incons} name="chevron-left" size={30} color="white" />
-                </TouchableOpacity>
-                    <View style={styles.textScreenName}>
-                        <Text style={styles.textScreenName}>
-                            {titleScreen}
-                        </Text>
-                    </View>
-                
-                </View>
+            <HeaderSimple
+                titleScreen={titleScreen}
+            />
             <View style={styles.container}>
                 <View style={styles.textContainer}>
                     <Text style={styles.textNameApp}>
@@ -50,22 +34,6 @@ const styles = StyleSheet.create({
       width: '100%',
       height: Dimensions.get('window').height * 0.15,
       justifyContent: 'center',
-    },
-    header:{
-        width: '100%',
-        height: Dimensions.get('window').height * 0.05,
-        backgroundColor: colors.blue,
-        flexDirection: 'row'   
-    },
-    incons: {
-        paddingLeft: 5
-    },
-    textScreenName: {
-        fontFamily: fonts.generic,
-        fontSize: 20,
-        paddingLeft: 15,
-        color: colors.white,
-        justifyContent: 'space-around'
     },
     textNameApp:{
         fontSize: 32,
