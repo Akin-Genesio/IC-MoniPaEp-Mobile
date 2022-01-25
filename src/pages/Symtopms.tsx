322e5d0b9c082209aa66bf8051d308862fe47e02
Vinicius-klaus <vinicius.genesio@usp.br>
2022-01-25 19:47:17 -0300

    feat(symptoms screen): create a search bar

--- src/pages/Symtopms.tsx
+++ src/pages/Symtopms.tsx
@@ -1,37 +1,87 @@
 import { MaterialIcons } from '@expo/vector-icons';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import {
-    Dimensions, Image,
-    Keyboard,
-    KeyboardAvoidingView,
+    Dimensions,
+
+
     StyleSheet,
     Text,
-    TouchableWithoutFeedback,
+
+    TextInput,
+
     View
 } from 'react-native';
-import patientImg from '../assets/patientImg.png';
-import { FAQ, GreenButton, Header, HeaderSimple, HeaderWithOutMenu, PatientStatus, SafeAreaView } from '../Components';
-import { useAuth } from '../contexts/Auth';
+import { HeaderSimple, SafeAreaView } from '../Components';
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 export function Symtopms(){
+    const[isSearchFocused, setIsSearchFocused] = useState(false)
+    const [isSearchFilled, setIsSearchFilled] = useState(false)
+    const [search, setSearch] = useState<string>('')
+    const searchRef = useRef(null)
+
+    //Functions handle for Search
+    function handleInputSearchBlur(){
+        setIsSearchFocused(false)
+        setIsSearchFilled(!!search)
+    }
+
+    function handleInputSearchFocus(){
+        setIsSearchFocused(true)
+    }
+
+    function handleInputSearchChange(value: string){
+        setIsSearchFilled(!!value)
+        setSearch(value)
+    }
+
+    function handleSeach(){
+        console.log(search)
+    }
 
     return(
-        <SafeAreaView>
+        <SafeAreaView 
+            accessible={true}
+        >
             <HeaderSimple
                 titleScreen = "Atualizar Sintomas"
             /> 
-
-                    <View style={styles.bodyUp}>
+                <View style={styles.container}>
+                    <View style={styles.bodyUp} accessible={true}>
                         <MaterialIcons style={styles.icons} name="menu" size={24} color="black" />
-                        <View style={styles.textAPP}>
+                        <View style={styles.textAPP} accessible={true}>
                             <Text style={styles.appName}>MoniPaEp</Text>
                         </View>
                     </View>
-                    <View>
-                        
+                    <View style={[
+                            styles.search,
+                            (isSearchFocused || isSearchFilled) && 
+                            {borderColor: colors.blue}
+                        ]}>
+                        <TextInput
+                            accessible={true}
+                            placeholder="Digite um sintoma"
+                            style={styles.textSerch}
+                            value={search}
+                            onBlur={handleInputSearchBlur}
+                            onFocus={handleInputSearchFocus}
+                            onChangeText={handleInputSearchChange}
+                        />
+                        <MaterialIcons 
+                            name="search" 
+                            size={24} 
+                            color="gray"
+                            style={[
+                                styles.Icon,
+                                (isSearchFocused || isSearchFilled) && 
+                                {color: colors.blue}
+                            ]}
+                            onPress={handleSeach} 
+                            />
+
                     </View>
+                </View>
         </SafeAreaView>
     )
 }
@@ -46,6 +96,7 @@ const styles = StyleSheet.create({
         width: '100%',
         height: Dimensions.get('window').height * 0.15,
         justifyContent: 'center',
+        paddingBottom: 15
     },
     icons: {
         padding: 20
@@ -57,5 +108,26 @@ const styles = StyleSheet.create({
     },
     textAPP: {
         alignItems: 'center',
-    }
+    },
+    search: {
+        flexDirection: 'row',
+        borderWidth: 1,
+        justifyContent: 'center',
+        width: Dimensions.get('window').width * 0.9,
+        backgroundColor: colors.gray_light1,
+        borderColor: colors.black,
+        height: 50,
+        borderRadius: 100,
+        
+        alignItems: 'center'
+    },
+    textSerch:{
+        color: colors.gray_dark2,
+        width: '70%',
+        fontFamily: fonts.generic,
+        fontSize: 16,
+    },
+    Icon:{
+        padding: 10,
+    },
 })
